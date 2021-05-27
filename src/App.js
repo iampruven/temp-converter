759e4103df8eac860714674b442ba6bdcff51b02
Vendy Prum <vendy@thinkful.com>
2021-05-27 14:13:45 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+function TempForm(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <form className="form-style">
+      <label htmlFor="degrees">Degrees</label>
+      <input
+        value={props.tempNum}
+        onChange={(e) => props.onSetTempNum(e.target.value)}
+        type="number"
+        name="degrees"
+        id="degrees"
+        className="in-style"
+      />
+      <label htmlFor="type">Type</label>
+      <select
+        value={props.tempType}
+        onChange={(e) => props.onTempType(e.target.value)}
+        className="in-style"
+      >
+        <option value="fahrenheit">Fahrenheit</option>
+        <option value="celcius">Celcius</option>
+      </select>
+      <button className="btn-style" onClick={(e) => props.handleCalculate(e)}>
+        Convert
+      </button>
+    </form>
+  );
+}
+function Header() {
+  return <h1 className="heading">Temperature Converter</h1>;
+}
+
+function Results(props){
+  return(<div className="results">
+  {props.newTemp && (
+    <p>
+      Results: {props.newTemp}{" "}
+      {props.tempType === "fahrenheit"
+        ? "degrees Celcius"
+        : "degrees Fahrenheit"}
+    </p>
+  )}
+</div>)
+}
+function App() {
+  const [tempNum, setTempNum] = useState("");
+  const [tempType, setTempType] = useState("celcius");
+  const [newTemp, setNewTemp] = useState("");
+
+  const handleCalculate = (e) => {
+    e.preventDefault();
+    if (tempType === "fahrenheit") {
+      //fahrenheit -> celcius
+      setNewTemp(Math.floor((tempNum - 32) * 0.5556));
+    } else {
+      //celcius->fahrenheit
+      console.log("teh", tempNum);
+      setNewTemp(Math.floor(tempNum * 1.8 + 32));
+    }
+    return setTempNum("");
+  };
+
+  return (
+    <>
+      <header>
+        <Header />
       </header>
-    </div>
+      <main className="container">
+        <div className="App">
+          <TempForm
+            tempNum={tempNum}
+            onSetTempNum={setTempNum}
+            handleCalculate={handleCalculate}
+            onTempType={setTempType}
+            tempType={tempType}
+          />
+          <Results newTemp={newTemp} tempType={tempType}/>
+        </div>
+      </main>
+    </>
   );
 }
 
